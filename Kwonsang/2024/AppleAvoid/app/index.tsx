46f4b47a342d31bc8f506ac1f4b28a8eac990a87
Moyeon <dbqld770@gmail.com>
2024-09-08 19:04:50 +0900

    240908

--- Kwonsang/2024/AppleAvoid/app/index.tsx
+++ Kwonsang/2024/AppleAvoid/app/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Dimensions, Text, View } from "react-native";
 import {
   GameEngine,
@@ -10,7 +11,14 @@ interface Entities {
 
 const { width, height } = Dimensions.get("window");
 
+const CHARACTER_SIZE = 60;
+const APPLE_SIZE = 40;
+const FLOOR = height - 100;
+
 export default function Index() {
+  const [characterDirection, setCharacterDirection] = useState(0);
+  const [isTouching, setIsTouching] = useState(0);
+
   const updateGame = (
     entities: Entities,
     { touches, time, dispatch }: GameEngineUpdateEventOptionType
@@ -18,19 +26,58 @@ export default function Index() {
     let character = entities.character;
 
     // TODO: move the character with touch
+    touches.forEach((t) => {
+      if (t.type === "start") {
+        setIsTouching(isTouching + 1);
+      } else if (t.type === "end") {
+        setIsTouching(isTouching - 1);
+        setCharacterDirection(0);
+      }
+
+      if (isTouching > 0) {
+        if (t.event.pageX < width / 2) {
+          // Move left
+          setCharacterDirection(-5);
+        } else {
+          // Move right
+          setCharacterDirection(5);
+        }
+      }
+    });
+
+    if (isTouching > 0) {
+      character.x += characterDirection;
+      if (character.x < 0) {
+        character.x = 0;
+      } else if (character.x > width - CHARACTER_SIZE) {
+        character.x = width - CHARACTER_SIZE;
+      }
+    }
+
+    character.renderer = <Character x={character.x} y={character.y} />;
+    return entities;
   };
+
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        position: "relative",
       }}
     >
       <GameEngine
         systems={[updateGame]}
         entities={{
-          character: { x: 100, y: 300, renderer: <Character /> },
+          background: {
+            width,
+            height,
+            renderer: <Background width={width} height={height} />,
+          },
+          character: {
+            x: 50,
+            y: FLOOR - CHARACTER_SIZE,
+            renderer: <Character x={50} y={FLOOR - CHARACTER_SIZE} />,
+          },
         }}
       ></GameEngine>
     </View>
@@ -41,13 +88,45 @@ const Character = ({ x, y }: { x: number; y: number }) => {
   return (
     <View
       style={{
-        width: 50,
-        height: 50,
+        width: CHARACTER_SIZE,
+        height: CHARACTER_SIZE,
         backgroundColor: "blue",
         position: "absolute",
         left: x,
         top: y,
       }}
     ></View>
+  );
+};
+
+// TODO: Apple spawning
+const Apple = ({ x, y }: { x: number; y: number }) => {
+  return (
+    <View
+      style={{
+        width: APPLE_SIZE,
+        height: APPLE_SIZE,
+        backgroundColor: "red",
+        position: "absolute",
+        borderRadius: APPLE_SIZE,
+        left: x,
+        top: y,
+      }}
+    ></View>
+  );
+};
+
+const Background = ({ width, height }: { width: number; height: number }) => {
+  return (
+    <View
+      style={{
+        width: width,
+        height: height,
+        position: "absolute",
+        top: 0,
+        left: 0,
+        backgroundColor: "lightgreen",
+      }}
+    />
   );
 };
